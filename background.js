03a0ebd4fd6b9e29046fd601ef68d1ecbeaab0ed
doraDemon <zliao@sfu.ca>
2014-05-24 19:42:12 -0700

    实现用firebase存取弹幕

--- background.js
+++ background.js
@@ -17,6 +17,8 @@ var danmaku = "";
 var dmnum = 1;
 var lol,awesome,OMG;
 var youtube = new Firebase('https://incandescent-fire-5345.firebaseio.com/video/youtube');
+var youtubeID = $(".html5-main-video").attr("data-youtube-id");
+var thisvideo = youtube.child(youtubeID);
 
 function createDemo(){
 		lol = setInterval(function(){
@@ -52,6 +54,14 @@ function createNewDanmaku(d){
   	}	
 }
 
+function createNewDanmakuWithTime(d,t){
+	setInterval(function(){
+		if (Math.abs(document.getElementsByClassName("video-stream html5-main-video")[0].currentTime-t)<0.101){
+				createNewDanmaku(d);
+		}
+	},200);
+}
+
 $("#enter").click(function(){
       if ($("#textbox").val()!=""){
           danmaku=$("#textbox").val();
@@ -74,7 +84,6 @@ $("#enter").click(function(){
    //     			currentPageUrlIs = document.location.toString();
 			// }
    //        alert(currentPageUrlIs);
-   		var youtubeID = $(".html5-main-video").attr("data-youtube-id");
    		console.log(youtubeID);
           saveNewDanmaku(danmaku,document.getElementsByClassName("video-stream html5-main-video")[0].currentTime,youtubeID);
           $("#danmakuTextBox").val("");
@@ -82,14 +91,20 @@ $("#enter").click(function(){
       }
 });
 
+thisvideo.on('child_added', function(snapshot) {
+  var dmData = snapshot.val();
+  createNewDanmakuWithTime(dmData.text,dmData.time);
+});
+
+
 function saveNewDanmaku(d,t,url){
- var thisvideo = youtube.child(url);
  // var thisdmnum = thisvideo.child('dmnum');
  thisvideo.push({time:t, text:d});
  // thisdmnum.transaction(function(current_value) {
  //          return current_value + 1;
  //      });
 }
+
 
 function templateD(newid,count,content){
  var newHTML = '<div class=\"danmaku msg-frame\" id=\"'+newid+'\" style=\"top:'+25*count+'px;\">';
